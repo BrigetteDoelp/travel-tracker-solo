5cd9452d45f83a4dbb2c817465e49ebe422e1923
Brigette Doelp <brigette.doelp@gmail.com>
2020-09-18 11:55:38 -0700

    Add endpoints for POST requests in comments for easy reference

--- src/api.js
+++ src/api.js
@@ -1,33 +1,35 @@
 const api = {
-  getOneTraveler(id) {
+  fetchOneTraveler(id) {
     const soloTravelerDataApi = `https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers/${id}`
     const promise = fetch(soloTravelerDataApi)
       .then (response => response.json())
     return promise;
   },
 
-  getAllTravelers() {
+  fetchAllTravelers() {
     const travelerDataApi = 'https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers'
     const promise = fetch(travelerDataApi)
       .then (response => response.json())
     return promise;
   },
 
-  getAllTrips() {
+  fetchAllTrips() {
     const tripDataApi = 'https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips'
     const promise = fetch(tripDataApi)
       .then (response => response.json())
     return promise;
   },
 
-  getAllDestinations() {
+  fetchAllDestinations() {
     const destinationDataApi = 'https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations'
     const promise = fetch(destinationDataApi)
       .then (response => response.json())
     return promise;
   },
 
-
+  //post new trip https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips
+  //post new destination https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations
+  //post (modify) single trip https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/updateTrip	
 
 
 }
