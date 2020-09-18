ed19223779b3dc69e3793e1818a9c3386c345783
Brigette Doelp <brigette.doelp@gmail.com>
2020-09-18 10:23:22 -0700

    Write method to fetch all trip data

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -1,7 +1,14 @@
 const fetchAPI = {
-  getTravelers() {
+  getAllTravelers() {
     const travelerDataApi = 'https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers'
     const promise = fetch(travelerDataApi)
+      .then (response => response.json())
+    return promise;
+  },
+
+  getAllTrips() {
+    const tripDataApi = 'https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips'
+    const promise = fetch(tripDataApi)
       .then (response => response.json())
     return promise;
   },
