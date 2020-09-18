8da51d0de97c5f83e65318430fc62e0aa55d6bb7
Brigette Doelp <brigette.doelp@gmail.com>
2020-09-18 15:04:41 -0700

    Adjust to grab specific traveler based on id

--- src/api.js
+++ src/api.js
@@ -30,6 +30,6 @@ const api = {
   //post new trip https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips
   //post new destination https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations
   //post (modify) single trip https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/updateTrip
-
-
 }
+
+export default api
