8e30778f2da793c3b823a7362fe95ac56f541fcc
Brigette Doelp <brigette.doelp@gmail.com>
2020-09-18 09:57:37 -0700

    Write method to fetch traveler data

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -1,3 +1,11 @@
 const fetchAPI = {
-  
+  getTravelers() {
+    const travelerDataApi = 'https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers'
+    const promise = fetch(travelerDataApi)
+      .then (response => response.json())
+    return promise;
+  },
+
+
+
 }
