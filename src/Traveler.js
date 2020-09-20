ff67cec1f4c41736f6f2658ceeef4a5f625b200e
Brigette Doelp <brigette.doelp@gmail.com>
2020-09-19 20:40:05 -0700

    Find all trips of one traveler

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,9 +1,28 @@
+// const moment = require('moment')
+import moment from 'moment';
+
+
 class Traveler {
-  constructor(id, name, travelerType) {
-    this.id = id;
-    this.name = name;
-    this.travelerType = travelerType;
+  constructor(traveler, trips) {
+    this.id = traveler.id;
+    this.name = traveler.name;
+    this.travelerType = traveler.travelerType;
+    this.allTrips = trips;
+    this.travelersTrips = [];
   }
 
-  
+  findTrips() {
+    this.travelersTrips = this.allTrips.filter(trip => trip.userID === this.id)
+  };
+
+  pastTrips() {
+    let today = moment(Date.now())
+    this.findTrips()
+    console.log(this.travelersTrips)
+    this.travelersTrips.filter(trip => {
+      moment(trip.date).isBefore(today).add(trip.duration, 'day')
+    })
+  }
+
 }
+export default Traveler
