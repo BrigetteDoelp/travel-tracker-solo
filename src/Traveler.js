c331ff4cc90406548c72584869e864ced0a77dad
Brigette Doelp <brigette.doelp@gmail.com>
2020-09-20 10:10:51 -0700

    Fix moment js bug

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,9 +1,31 @@
+// const moment = require('moment')
+const moment = require('moment');
+
+
 class Traveler {
-  constructor(id, name, travelerType) {
-    this.id = id;
-    this.name = name;
-    this.travelerType = travelerType;
+  constructor(traveler, trips) {
+    this.id = traveler.id;
+    this.name = traveler.name;
+    this.travelerType = traveler.travelerType;
+    this.allTrips = trips;
+    this.travelersTrips = [];
   }
 
-  
+  findTrips() {
+    this.travelersTrips = this.allTrips.filter(trip => trip.userID === this.id)
+  };
+
+  pastTrips() {
+    let today = moment('2020/05/15').format('YYYY/MM/DD')
+    console.log(today)
+    this.findTrips()
+    console.log(this.travelersTrips)
+    let ass = this.travelersTrips.filter(trip => {
+      moment(trip.date).add(trip.duration, 'day').isBefore(today)
+  })
+  console.log(ass)
+
+  }
+
 }
+export default Traveler
