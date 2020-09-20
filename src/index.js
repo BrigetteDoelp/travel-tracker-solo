5d043cfae5fd49221f890d68dcd4d70de6e52c7c
Brigette Doelp <brigette.doelp@gmail.com>
2020-09-19 17:45:12 -0700

    Test moment functionality

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import moment from 'moment';
 
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
@@ -10,6 +11,7 @@ import './images/turing-logo.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');
 
+import updateDOM from './domUpdate.js'
 import api from './api.js'
 
 let userID = Math.floor((Math.random() * 50) + 1);
@@ -23,6 +25,7 @@ function onLoadContent() {
   let promise1 = api.fetchAllTravelers();
   let promise2 = api.fetchAllTrips();
   let promise3 = api.fetchAllDestinations();
+  console.log(moment().format('MMMM Do YYYY'));
 
   Promise.all([promise1, promise2, promise3])
     .then(values => {
@@ -32,3 +35,21 @@ function onLoadContent() {
       destinations = values[2].destinations;
     })
 }
+
+//GENERAL FUNCTIONS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
