46cbab2e41e83a4b68016219773686b5dc21405f
Brigette Doelp <brigette.doelp@gmail.com>
2020-09-20 13:04:48 -0700

    Begin test for pastTrips() method

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -8,7 +8,7 @@ describe('Traveler', () => {
   let traveler;
 
   beforeEach(() => {
-    traveler = new Traveler(travelerData[0], tripsData);
+    traveler = new Traveler(travelerData[0]);
   });
 
   it('should be a function', () => {
@@ -18,8 +18,13 @@ describe('Traveler', () => {
   it('should be an instance of Traveler', () => {
     expect(traveler).to.be.an.instanceof(Traveler);
   })
-
-  
+  //
+  // it('should find past trips of a traveler', () => {
+  //   console.log(traveler.pastTrips())
+  //   // expect(traveler.pastTrips()).to.deep.equal([
+  //   //   {"id":3,"userID":1,"destinationID":22,"travelers":4,"date":"2020/05/22","duration":17,"status":"pending","suggestedActivities":[]},
+  //   // ])
+  // })
 
 
 
