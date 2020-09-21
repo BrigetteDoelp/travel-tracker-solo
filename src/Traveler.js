a596a953aee8cd7d58ca94a5ea53a8215f97938a
Brigette Doelp <brigette.doelp@gmail.com>
2020-09-20 17:31:01 -0700

    Correctly import traveler trip data

--- src/Traveler.js
+++ src/Traveler.js
@@ -57,6 +57,10 @@ class Traveler {
     return thisYearsTrips
   }
 
+  moneySpent() {
+    
+  }
+
 
 
 
--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -9,6 +9,17 @@ describe('Traveler', () => {
 
   beforeEach(() => {
     traveler = new Traveler(travelerData[0]);
+    traveler.travelersTrips = [
+      {"id":1,"userID":1,"destinationID":49,"travelers":1,"date":"2019/09/16","duration":8,"status":"approved","suggestedActivities":[]},
+      {"id":2,"userID":1,"destinationID":25,"travelers":5,"date":"2020/10/04","duration":18,"status":"pending","suggestedActivities":[]},
+      {"id":3,"userID":1,"destinationID":22,"travelers":4,"date":"2020/05/22","duration":17,"status":"pending","suggestedActivities":[]},
+      {"id":4,"userID":1,"destinationID":14,"travelers":2,"date":"2019/02/25","duration":10,"status":"approved","suggestedActivities":[]},
+      {"id":5,"userID":1,"destinationID":29,"travelers":3,"date":"2020/04/30","duration":18,"status":"approved","suggestedActivities":[]},
+      {"id":6,"userID":1,"destinationID":35,"travelers":3,"date":"2021/06/29","duration":9,"status":"approved","suggestedActivities":[]},
+      {"id":7,"userID":1,"destinationID":17,"travelers":5,"date":"2019/5/28","duration":20,"status":"approved","suggestedActivities":[]},
+      {"id":8,"userID":1,"destinationID":39,"travelers":6,"date":"2021/02/07","duration":4,"status":"pending","suggestedActivities":[]},
+      {"id":9,"userID":1,"destinationID":19,"travelers":5,"date":"2019/12/19","duration":19,"status":"approved","suggestedActivities":[]},
+    ]
   });
 
   it('should be a function', () => {
@@ -32,7 +43,15 @@ describe('Traveler', () => {
   })
 
   it('should be able to check for pending trips', () => {
-    
+
+  })
+
+  it('should be able to get a full year of trips', () => {
+
+  })
+
+  it('should be able to calculate the total amount spent on trips for that year', () => {
+
   })
 
 })
