b8fd2080c1c22b3c4fd4e05861fc56be1adbe3b7
Brigette Doelp <brigette.doelp@gmail.com>
2020-09-24 01:25:19 -0600

    Fix POST bug

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ let traveler;
 let trips;
 let destinations;
 let requestedTrip;
-let userID = Math.floor((Math.random() * 50) + 1);
+let generatedTraveler;
+// let userID = Math.floor((Math.random() * 50) + 1);
 let submitBtn = document.querySelector('.submit-button');
 let errorMessage = document.querySelector('.error-message');
 let dateInput = document.querySelector('.date-input')
@@ -58,31 +59,71 @@ function validateLogin() {
 
 function getTraveler(userID) {
   api.fetchOneTraveler(userID)
+  console.log(userID)
   let traveler = generateTraveler(userID)
   return traveler
 }
 
-function generateTraveler(userID) {
+function generateTraveler(traveler) {
   let soloTraveler = new Traveler(traveler);
+  soloTraveler.id = traveler.id
   generateTravelerTripData(soloTraveler);
   findYearOfDestinations(soloTraveler);
   generateTripCosts(soloTraveler);
-  soloTraveler.id = userID
+  generatedTraveler = soloTraveler;
+  console.log(generatedTraveler)
   return soloTraveler
 }
 
 function onSubmitData() {
   let promise0 = api.fetchAllTrips();
   let promise1 = api.fetchAllDestinations();
-  let promise2 = api.fetchOneTraveler(userID);
+  let promise2 = api.fetchOneTraveler(generatedTraveler.id);
   Promise.all([promise0, promise1, promise2])
     .then(values => {
       trips = values[0].trips;
       destinations = values[1].destinations;
       traveler = values[2]
-      let newTraveler = generateTraveler(30)
-      onLoadDisplay(newTraveler, destinations)
+      let newTraveler = generateTraveler(generatedTraveler)
+      // onLoadDisplay(newTraveler, destinations)
+      // updateDom.updatePendingTrips(newTraveler, destinations)
     })
+}
+
+function postTrip() {
+  let postTrip = requestedTrip
+  let postRequest = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(postTrip)
+  };
+  let postedTrip = api.fetchNewTrip(postRequest);
+  let promise0 = api.fetchOneTraveler(generatedTraveler.id);
+  let promise1 = api.fetchAllDestinations();
+  Promise.all([postedTrip, promise0, promise1])
+    .then(onSubmitData())
+    .then(values => {
+      let traveler = values[1]
+      let allDestinations = values[2]
+      let newTraveler = generateTraveler(traveler)
+      let tripCost = generateNewTripCost(requestedTrip, allDestinations)
+      updateDom.displayNewTripCost(tripCost)
+      newTraveler.travelersTrips.push(requestedTrip)
+      updateDom.updatePendingTrips(newTraveler, destinations)
+    })
+}
+
+function submitTrip() {
+  if(validateDateEntry() && validateDuration() && validateTravelers() && validateDestination() === true) {
+    let trip = generateNewTrip()
+    postTrip()
+    clearInput()
+    errorMessage.classList.add('hidden')
+  } else {
+    errorMessage.classList.remove('hidden')
+  }
 }
 
 function clearInput() {
@@ -94,9 +135,9 @@ function clearInput() {
   partySizeInput.value = '';
 }
 
-function fetchUserData(travelerID) {
-  let user = api.fetchOneTraveler(travelerID)
-}
+// function fetchUserData(travelerID) {
+//   let user = api.fetchOneTraveler(travelerID)
+// }
 
 function getDestinations(destinations) {
   let allDestinations = destinations
@@ -113,21 +154,10 @@ function onLoadDisplay(traveler, destinations) {
   updateDom.displayMoneySpent(traveler);
 }
 
-function submitTrip() {
-  if(validateDateEntry() && validateDuration() && validateTravelers() && validateDestination() === true) {
-    let trip = generateNewTrip()
-    postTrip()
-    clearInput()
-    errorMessage.classList.add('hidden')
-  } else {
-    errorMessage.classList.remove('hidden')
-  }
-}
-
 function generateNewTrip() {
   let newTrip = {
     id: Math.floor((Math.random() * 100) + 50),
-    userID: userID,
+    userID: generatedTraveler.id,
     destinationID: parseInt(dropdown.value),
     travelers: parseInt(travelerInput.value),
     date: dateInput.value,
@@ -152,30 +182,7 @@ function convertTripForPost(trip) {
   }
 }
 
-function postTrip() {
-  let postTrip = requestedTrip
-  let postRequest = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(postTrip)
-  };
-  let postedTrip = api.fetchNewTrip(postRequest);
-  let promise0 = api.fetchOneTraveler(userID);
-  let promise1 = api.fetchAllDestinations();
-  Promise.all([postedTrip, promise0, promise1])
-    .then(onSubmitData())
-    .then(values => {
-      let traveler = values[1]
-      let allDestinations = values[2]
-      let newTraveler = generateTraveler()
-      let tripCost = generateNewTripCost(requestedTrip, allDestinations)
-      updateDom.displayNewTripCost(tripCost)
-      newTraveler.travelersTrips.push(requestedTrip)
-      updateDom.updatePendingTrips(newTraveler, destinations)
-    })
-}
+
 
 function generateTravelerTripData(traveler) {
   let travelerDestinations = [];
@@ -226,7 +233,6 @@ function generateTripCosts(traveler) {
 
 function generateNewTripCost(passedInTrip, destinations) {
   let trip = passedInTrip
-  console.log(destinations)
   let singleDestination = destinations.destinations.find(destination => {
     return destination.id === trip.destinationID
   });
